3cfcc3a063b6a269390060855232b150fcae797c
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-03-16 13:52:09 +0900

    앱업데이트
    요청자 : 노성래
    
    수정자 : 노진국

--- application/views/kor/recipe_list.php
+++ application/views/kor/recipe_list.php
@@ -121,7 +121,7 @@
 					<div class="col-sm-8">
                                             <select id="insert_recipe_group" name="insert_recipe_group">
                                                 <?php
-                                                    var_dump($group_rows);
+                                                    var_dump($grow);
                                                     foreach($group_rows as $grow){
                                                         echo "<option value='".$grow->idx."'>".$grow->name."</option>";
                                                     }
