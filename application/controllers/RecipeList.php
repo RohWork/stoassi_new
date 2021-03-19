c1c9534553e5ae373ba36439585e3e236787c5b5
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-03-19 15:25:37 +0900

    앱업데이트
    요청자 : 노성래
    
    수정자 : 노진국

--- application/controllers/RecipeList.php
+++ application/controllers/RecipeList.php
@@ -84,7 +84,6 @@ class RecipeList extends CI_Controller {
         $data['scategory_select'] = $scategory_select;
         $data['stock_info'] = $stock_info;
         $data['stock_data'] = $stock_data;
-        $data['stock_select_key'] = array_keys($stock_select);
         
         $this->load->view(LANGUAGE.'/header', $this->head_data);
         $this->load->view(LANGUAGE.'/recipe_list', $data);
