ac7d9b0b2db6af9afa52f2cda298776665ef0fcf
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-03-19 16:20:10 +0900

    앱업데이트
    요청자 : 노성래
    
    수정자 : 노진국

--- application/views/kor/recipe_list.php
+++ application/views/kor/recipe_list.php
@@ -212,12 +212,12 @@
         $("#stock_info").html("");
         
         var stock_category_data = stock_info_array[idx];
-        var stock_select ="";
+        var stock_select = "";
         
         for(var i=0;i<stock_category_data.length;i++){
             var row = stock_category_data;
             
-            stock_select .= "<option value="+row['idx']+">"+row['name']+"</optio>";
+            stock_select += "<option value="+row['idx']+">"+row['name']+"</optio>";
         }
         
         $("#stock_info").html(stock_select);
