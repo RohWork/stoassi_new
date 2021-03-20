6d28a9099b34fd9b1aeaea82cbefae91d01c7105
ROH <ROH@125.133.43.195>
2021-03-20 19:33:54 +0900

    레시피관리추가
    요청자 : 노진국

--- application/models/Recipe_model.php
+++ application/models/Recipe_model.php
@@ -95,7 +95,7 @@ class Recipe_model extends CI_Model {
     function insert_recipe($vo){
         
         $data['regi_date'] = $data['modi_date'] =  date('Y-m-d H:i:s');
-        $data['writer'] = $data['modfier'] = $this->session->userdata("user_id");
+        $data['writer'] = $data['modifier'] = $this->session->userdata("user_id");
         $data['group_idx'] = $vo['group_idx'];
         $data['name'] = $vo['name'];
         
