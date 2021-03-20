6dc65b1c157501c1fe26e6d11806fcf14ad694a5
ROH <ROH@125.133.43.195>
2021-03-20 20:28:37 +0900

    레시피관리추가
    요청자 : 노진국

--- application/controllers/RecipeGroup.php
+++ application/controllers/RecipeGroup.php
@@ -3,7 +3,8 @@ class RecipeGroup extends CI_Controller {
 	
     function __construct() {
         parent ::__construct();
-
+        $this->allow=array();
+        
         $this->head_data = header_set("recipe_group");
 
         $this->load->model('Recipe_model', 'recipe_md', TRUE);
