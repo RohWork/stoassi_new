80771b4a8b923d6a47d9a3dc3d01d24a52b5b5b4
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-03-29 17:02:21 +0900

    메시지 기능추가
    요청자 : 제혜민
    
    수정자 : 노진국

--- application/views/kor/recipe_list.php
+++ application/views/kor/recipe_list.php
@@ -407,7 +407,7 @@
         
         recipe_val.append(recipe_html);
      
-        $("#stock_category_update"+stock_info_cnt).val(category_idx);
+        $("#stock_category_"+mode+""+stock_info_cnt).val(category_idx);
         stock_info_set(category_idx,stock_info_cnt);    
         stock_unit_set($("#stock_info_"+mode+""+stock_info_cnt).val(),stock_info_cnt);   
     }
