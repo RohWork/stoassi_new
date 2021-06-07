5371dc95fcd49a629a0ed2f814065ebb82ed66ff
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-06-07 14:57:00 +0900

    렛츠고파닉스 NEW LMS 쪽지기능수정
    요청자 : 노성래 팀장
    
    수정자 : 노진국

---
+++ application/views/orderMenu1.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 2 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Refobi</title>
+        <!-- 합쳐지고 최소화된 최신 CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+        <!-- 부가적인 테마 -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <!-- Custom styles for this template -->
+        <link href="../../assets/css/sticky-footer-navbar.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-4"></div>
+                <div class="col-xs-4"><center><h3>가게명</h3></center></div>
+                <div class="col-xs-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-4 col-xs-6">LANGUAGE1</div>
+                <div class="col-md-4 col-xs-6">LANGUAGE2</div>
+                <div class="col-md-2"></div>
+            </div>
+        </div>
+    </body>
+</html>
+        
+        
