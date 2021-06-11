29c4f5f684f042c3cfd9b8423655efbcd134a4cc
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-06-11 09:55:59 +0900

    렛츠고파닉스 NEW LMS 쪽지기능수정
    요청자 : 노성래 팀장
    
    수정자 : 노진국

--- application/views/kor/orderMenu1.php
+++ application/views/kor/orderMenu1.php
@@ -25,7 +25,7 @@
                         <div class="col-xs-4"><center><h3>가게명</h3></center></div>
                         <div class="col-xs-2"></div>
                     </div>
-                    <div class="row" style="margin-top: 30vx">
+                    <div class="row" style="margin-top: 30px">
                         <div class="col-md-2"></div>
                         <div class="col-md-4 col-xs-6">LANGUAGE1</div>
                         <div class="col-md-4 col-xs-6">LANGUAGE2</div>
