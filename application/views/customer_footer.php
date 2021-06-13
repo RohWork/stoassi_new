d78c177c61913c50170628a22f231e0d8247b2a7
ROH <ROH@125.133.43.195>
2021-06-13 17:32:13 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -1,10 +1,19 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-2 col-xs-2"></div>
-            <div class="col-md-4 col-xs-4" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
-            <div class="col-md-4 col-xs-4" style="text-align: center" onclick="history.back();">뒤로가기</div>
-            <div class="col-md-2 col-xs-2"></div>
+            <div class="col-md-2 col-xs-3"></div>
+            <div class="col-md-4 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
+            <div class="col-md-4 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
+            <div class="col-md-4 col-xs-2">
+                <select>
+                    <?php
+                        foreach($language_list as $lang){
+                            echo "<option value=".$lang['language'].">".$lang['language_full']."</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="col-md-2 col-xs-3"></div>
         </div>
     </div>
 </footer>
