dc52d9499720ad97e3381fe033492359b3554209
ROH <ROH@125.133.43.195>
2021-06-13 15:24:29 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -56,7 +56,7 @@
     <script>
         function menu_check_go(idx, element){
             $(element).css('backgroundColor' , '#CCCCCC');
-            $("#menu_idx").append("/"+idx);
+            $("#menu_idx").val(this.val()+idx);
             
            // $("#orderForm")[0].submit();
             
