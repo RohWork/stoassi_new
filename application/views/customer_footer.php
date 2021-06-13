3d7a8474dfeca264669bacfd7e0694d61c70573e
ROH <ROH@125.133.43.195>
2021-06-13 17:34:54 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-2 col-xs-3"></div>
-            <div class="col-md-4 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
-            <div class="col-md-4 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
-            <div class="col-md-4 col-xs-2">
+            <div class="col-md-3 col-xs-3"></div>
+            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
+            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
+            <div class="col-md-2 col-xs-2">
                 <select>
                     <option value="">언어변경</option>
                     <?php
@@ -14,7 +14,7 @@
                     ?>
                 </select>
             </div>
-            <div class="col-md-2 col-xs-3"></div>
+            <div class="col-md-3 col-xs-3"></div>
         </div>
     </div>
 </footer>
