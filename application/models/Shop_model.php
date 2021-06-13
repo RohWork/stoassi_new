4203d3c460935b38f1dc1bc1d77b60a8f1238f5f
ROH <ROH@125.133.43.195>
2021-06-13 17:16:50 +0900

    레시피관리추가
    요청자 : 노진국

--- application/models/Shop_model.php
+++ application/models/Shop_model.php
@@ -37,7 +37,7 @@ class Shop_model extends CI_Model {
         $this->db->from("shop_setting");
         $this->db->where("shop_idx", $shop_idx);
         
-        return $this->db->get()->result_array();
+        return $this->db->get()->result();
     }
 }
 
