a57bffa14c19f90435463b54bd3f40ab04e33204
ROH <ROH@125.133.43.195>
2021-06-13 17:58:14 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -14,8 +14,9 @@
                             }
                         ?>
                     </select>
-                    <input type="hidden" name="place" id="place" value="<?=$place?>"/>
-                    <input type="hidden" name="menu_idx" id="menu_idx" value="<?=$menu_idx?>"/>
+
+                    <input type="hidden" name="place" id="place" value="<?= !empty($place)? $place : "" ?>"/>
+                    <input type="hidden" name="menu_idx" id="menu_idx" value="<?= !empty($menu_idx)? $place : ""?>"/>
                 </form>
             </div>
             <div class="col-md-3 col-xs-3"></div>
