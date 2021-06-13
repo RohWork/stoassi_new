b334377201d21c7f9f64727d053b2e91e84e5798
ROH <ROH@125.133.43.195>
2021-06-13 14:34:39 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/kor/orderMenu4.php
+++ application/views/kor/orderMenu4.php
@@ -38,7 +38,7 @@
                                                 if($cnt % 2  == 0){
                                                     echo "</tr><tr>";
                                                 }
-                                                            echo "<td align='center' style='cursor:pointer' onclick='submenu_go(".$menu->idx.")'><b>".$menu->name."<b/></td>";
+                                                            echo "<td align='center' onclick='submenu_go(".$menu->idx.")'><b style='cursor:pointer'>".$menu->name."<b/></td>";
                                                 $cnt++;
                                             }
                                         ?>  
