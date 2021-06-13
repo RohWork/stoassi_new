67a7288e5bba1a05662773e6ddb9be1b51ce3d7d
ROH <ROH@125.133.43.195>
2021-06-13 16:54:06 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/kor/orderMenu2.php
+++ application/views/kor/orderMenu2.php
@@ -50,8 +50,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2 col-xs-2"></div>
-                <div class="col-md-4 col-xs-4" style="text-align: center">HOME</div>
-                <div class="col-md-4 col-xs-4" style="text-align: center">뒤로가기</div>
+                <div class="col-md-4 col-xs-4" style="text-align: center" onclick="location.href='/orderMenu/1/10'">HOME</div>
+                <div class="col-md-4 col-xs-4" style="text-align: center" onclick="history.back();">뒤로가기</div>
                 <div class="col-md-2 col-xs-2"></div>
             </div>
 	</div>
