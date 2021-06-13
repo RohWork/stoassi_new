25db9f7f7072723578377d2533817ffb807f3ba6
ROH <ROH@125.133.43.195>
2021-06-13 16:48:32 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/kor/orderMenu2.php
+++ application/views/kor/orderMenu2.php
@@ -47,7 +47,14 @@
         }
     </script>
     <footer>
-        test
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-xs-2"></div>
+                <div class="col-md-4 col-xs-4">HOME</div>
+                <div class="col-md-4 col-xs-4">뒤로가기</div>
+                <div class="col-md-2 col-xs-2"></div>
+            </div>
+	</div>
     </footer>
 </html>
         
