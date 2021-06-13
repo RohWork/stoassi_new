b25f6897efec638acf097132460e6348e311c44e
ROH <ROH@125.133.43.195>
2021-06-13 17:29:37 +0900

    레시피관리추가
    요청자 : 노진국

---
+++ application/views/customer_footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-xs-2"></div>
+            <div class="col-md-4 col-xs-4" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
+            <div class="col-md-4 col-xs-4" style="text-align: center" onclick="history.back();">뒤로가기</div>
+            <div class="col-md-2 col-xs-2"></div>
+        </div>
+    </div>
+</footer>
