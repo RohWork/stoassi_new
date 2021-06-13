9bc20f284535fea7752beb3864fea82eb931c7a8
ROH <ROH@125.133.43.195>
2021-06-13 17:37:18 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-xs-3"></div>
-            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
-            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
+            <div class="col-md-2 col-xs-2 form-control" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
+            <div class="col-md-2 col-xs-2 form-control" style="text-align: center" onclick="history.back();">뒤로가기</div>
             <div class="col-md-2 col-xs-2">
-                <select class="input-group">
+                <select class="form-control">
                     <option value="">언어변경</option>
                     <?php
                         foreach($language_list as $lang){
