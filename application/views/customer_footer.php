1786eab67696e97a21ef0b86399e8f5765600476
ROH <ROH@125.133.43.195>
2021-06-13 17:53:08 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -5,16 +5,24 @@
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
             <div class="col-md-2 col-xs-2">
-                <select class="form-control">
-                    <option value="">언어변경</option>
-                    <?php
-                        foreach($language_list as $lang){
-                            echo "<option value='".$lang->language."'>".$lang->language_full."</option>";
-                        }
-                    ?>
-                </select>
+                <form method="post" id="change_form">
+                    <select class="form-control" id="lang_change" name="language">
+                        <option value="">언어변경</option>
+                        <?php
+                            foreach($language_list as $lang){
+                                echo "<option value='".$lang->language."'>".$lang->language_full."</option>";
+                            }
+                        ?>
+                    </select>
+                </form>
             </div>
             <div class="col-md-3 col-xs-3"></div>
         </div>
     </div>
 </footer>
+
+<script>
+    $("#lang_change").on("change", function(){
+        $("#change_form")[0].submit();
+    });
+</script>
