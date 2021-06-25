0de99f9da71b96e622dd43fd0adb68076984fc71
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-06-25 10:42:54 +0900

    모바일 페이지 차단
    요청자 : 송진솔연구원
    
    수정자 : 노진국

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -38,6 +38,21 @@ class Customer_model extends CI_Model {
         $this->db->like('ol.regi_date', $vo->date);
         return $this->db->count_all_results();
     }
+    
+    function detail_order($vo){
+        
+        $this->db->select("if(ol.place = 1 , '취식' , '포장') AS place, ol.table_no, ol.cnt");
+        $this->db->select("ol.regi_date, if(ol.`status` = 1, '결제대기', if(ol.status=2,'결제완료','조리완료') ) AS STATUS");
+        $this->db->select("ri.name AS recipe_name , rg.name AS group_name, ri.idx");
+        $this->db->from('order_list AS ol');
+        $this->db->join('recipe_info AS ri ', 'ol.recipe_idx = ri.idx', 'left');
+        $this->db->join('recipe_group AS rg', 'ri.group_idx = rg.idx', 'left');
+        
+        $this->db->where('ri.idx', $vo->idx);
+        
+        return $this->db->get()->row();
+        
+    }
 }
 ?>
 
