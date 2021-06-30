adf9354f3b6b226ba04cbf142211e0aa6836f5e0
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-06-30 13:51:03 +0900

    모바일 페이지 차단
    요청자 : 송진솔연구원
    
    수정자 : 노진국

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -103,6 +103,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             $this->load->library('pagination');
             $vo = new stdClass();
             $vo->date = date('Y-m-d');
+            $vo->status = 2;
             $offset = $this->input->get('per_page');
             
             $config['total_rows'] = $this->cust_md->count_order($vo);
