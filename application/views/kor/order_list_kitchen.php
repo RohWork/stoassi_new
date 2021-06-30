732a5cb4dfc300e7612eb08f6f5219070c06ebbf
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-06-30 13:44:02 +0900

    모바일 페이지 차단
    요청자 : 송진솔연구원
    
    수정자 : 노진국

--- application/views/kor/order_list_kitchen.php
+++ application/views/kor/order_list_kitchen.php
@@ -9,11 +9,10 @@
 				<thead>
 					<tr>
 						<th>NO</th>
-						<th>테이블번호</th>
 						<th>주문상태</th>
 						<th>주문시각</th>
+                                                <th>메뉴타입</th>
 						<th>주문명</th>
-						<th>포장여부</th>
 						<th>결제여부/수정</th>
 					</tr>
 				</thead>
@@ -24,12 +23,11 @@
 				?>
 					<tr>
 						<td><?=$no?></td>
-						<td><?=$row->table_no?></td>
 						<td><?=$row->STATUS?></td>
 						<td><?=$row->regi_date?></td>
-						<td><?=$row->group_name?><?=$row->recipe_name?></td>
-						<td><?=$row->place?></td>
-						<td><button type="button" id="modi_button" onclick="detail_order_show('<?=$row->idx?>')" class="btn btn-default">확인/수정</button></td>
+						<td><?=$row->group_name?></td>
+                                                <td><?=$row->recipe_name?></td>
+						<td><button type="button" id="modi_button" onclick="detail_order_show('<?=$row->idx?>')" class="btn btn-default">조리진행</button></td>
 					</tr>
 				<?php
 				$no ++;
