e62cc7c3bbafcd15f6597673ee69c6d09743cd9e
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-07-15 14:37:43 +0900

    주간데이터 수정
    요청자 : 노성래팀장
    
    수정자 : 노진국

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -53,7 +53,7 @@ class Customer_model extends CI_Model {
     function detail_order($vo){
         
         $this->db->select("if(ol.place = 1 , '취식' , '포장') AS place, ol.table_no, ol.cnt");
-        $this->db->select("ol.regi_date, ol.status");
+        $this->db->select("ol.regi_date, ol.status,");
         $this->db->select("ri.name AS recipe_name , rg.name AS group_name, ol.idx, ri.idx as recipe_idx");
         $this->db->from('order_list AS ol');
         $this->db->join('recipe_info AS ri ', 'ol.recipe_idx = ri.idx', 'left');
