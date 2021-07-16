f0d1bda6a0a5c7c213cb54fa0063c4099ce36ae2
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-07-16 09:37:56 +0900

    주간데이터 수정
    요청자 : 노성래팀장
    
    수정자 : 노진국

--- application/views/kor/order_list_kitchen.php
+++ application/views/kor/order_list_kitchen.php
@@ -74,7 +74,7 @@
 				</div>
                                 <div class="form-group" >
                                     <div class="col-sm-12">
-                                        <table id="recipe_area">
+                                        <table id="recipe_area" border="0" cellpadding="0" cellspacing="0">
                                             <th>
                                                 <td>조리순서</td>
                                                 <td>재료명</td>
