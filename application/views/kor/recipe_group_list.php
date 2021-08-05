1bdd0479c50b0a9365e5feb99cadd7df8c228bea
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-05 09:44:05 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

--- application/views/kor/recipe_group_list.php
+++ application/views/kor/recipe_group_list.php
@@ -69,7 +69,7 @@
                                 <div class="form-group">
 					<label for="stock_name" class="col-sm-3 control-label">부가세(%)</label>
 					<div class="col-sm-3">
-                                                <label class="radio-inline">
+                                                <label>
 						<input type="text" id="update_group_tax" name="update_group_tax" class="form-control"/>%
                                                 </label>
 					</div>
