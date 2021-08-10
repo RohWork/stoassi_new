3620c1754369aaccb0b0be541248442ebedb5260
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-10 10:06:11 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -3,11 +3,11 @@
         <div class="row">
             <div class="col-md-3 col-xs-3"></div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
-            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
+            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">BACK</div>
             <div class="col-md-2 col-xs-2">
                 <form method="post" id="change_form">
                     <select class="form-control" id="lang_change" name="language">
-                        <option value="">언어변경</option>
+                        <option value="">LANGUAGE</option>
                         <?php
                             foreach($language_list as $lang){
                                 echo "<option value='".$lang->language."'>".$lang->language_full."</option>";
@@ -23,6 +23,11 @@
         </div>
     </div>
 </footer>
+<div style="position: absolute">
+    test
+    
+</div>
+
 
 <script>
     $("#lang_change").on("change", function(){
