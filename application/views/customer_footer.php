298de8257522eab58754700c2e5f10bfb9186f3f
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-10 15:20:59 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -4,6 +4,7 @@
             <div class="col-md-3 col-xs-3"></div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">BACK</div>
+            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="$('#myModal').modal('show');">BASKET</div>
             <div class="col-md-2 col-xs-2">
                 <form method="post" id="change_form">
                     <select class="form-control" id="lang_change" name="language">
@@ -23,9 +24,24 @@
         </div>
     </div>
 </footer>
-<div style="position: absolute">
-    test
-    
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 
 
