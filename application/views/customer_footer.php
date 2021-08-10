5bd29e5d8f35899ac93307bc9f3c1493c760f100
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-10 15:37:07 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

--- application/views/customer_footer.php
+++ application/views/customer_footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-3 col-xs-3"></div>
+            <div class="col-md-1 col-xs-1"></div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">BACK</div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="$('#myModal').modal('show');">BASKET</div>
