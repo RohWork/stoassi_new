e2de6b043e1603d720ed263679c266ea165a63d8
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-11 13:55:16 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

---
+++ application/views/eng/customer_footer.php
@@ -0,0 +1,52 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
+            <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">BACK</div>
+            <div class="col-md-2 col-xs-3" style="text-align: center" onclick="$('#myModal').modal('show');">BASKET</div>
+            <div class="col-md-2 col-xs-5">
+                <form method="post" id="change_form">
+                    <select class="form-control" id="lang_change" name="language">
+                        <option value="">LANGUAGE</option>
+                        <?php
+                            foreach($language_list as $lang){
+                                echo "<option value='".$lang->language."'>".$lang->language_full."</option>";
+                            }
+                        ?>
+                    </select>
+
+                    <input type="hidden" name="place" id="place" value="<?= !empty($place)? $place : "" ?>"/>
+                    <input type="hidden" name="menu_idx" id="menu_idx" value="<?= !empty($menu_idx)? $place : ""?>"/>
+                </form>
+            </div>
+            <div class="col-md-2 "></div>
+        </div>
+    </div>
+</footer>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script>
+    $("#lang_change").on("change", function(){
+        $("#change_form")[0].submit();
+    });
+</script>
