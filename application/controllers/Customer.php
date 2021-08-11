dec7d4ca83655ef286184f80f3f044254b600595
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-11 15:20:55 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -34,7 +34,7 @@ class Customer extends CI_Controller {
                     "table_no" => $table_no,
                 );
                 
-                set_cookie($params);   //테이블번호, 혹은 시리얼넘버는 변조가 안되도록 세션으로 처리
+                set_cookie($params);   //테이블번호, 혹은 시리얼넘버는 변조가 안되도록 쿠키으로 처리
             }
             if(empty($shop_idx) || $shop_idx == 0){
                 show_error("Check to Your URL."); 
