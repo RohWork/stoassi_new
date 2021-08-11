99ff2162ad267a60c32b1cec2c6e0506b6467c0d
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-11 14:39:37 +0900

    플레이어 변경
    요청자 : 노진국
    
    수정자 : 노진국

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -93,7 +93,7 @@
                 $(element).css('backgroundColor' , '#CCCCCC');
                 $("#menu_idx").val($("#menu_idx").val()+"/"+idx);
                 $("#menu_list").append(
-                        "<tr id='tr_"+idx+"'><td>"+no+"</td><td>"+menuArray[idx]+"</td><td><input type='number'></td></tr>"
+                        "<tr id='tr_"+idx+"'><td>"+no+"</td><td>"+menuArray[idx]+"</td><td><input type='number' class=form-control></td></tr>"
                 );
         
                 no++;
