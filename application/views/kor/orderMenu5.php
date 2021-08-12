e07bba9c5511182129f51157d0e76d2fb911d2e7
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-12 13:16:03 +0900

    iframe 테스트 url생성
    요청자 : 노성래
    
    수정자 : 노진국

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -61,7 +61,7 @@
                             <th width="20%">갯수</th>
                         </tr>
                     </table>
-                    <div style="width: 100%; margin-left: 30px">
+                    <div style="width: 100%; margin-left: 30%">
                         <label> 메뉴가격 :  </label><span id="sum"> </span><br/>
                         <label> 부가세 : </label><span id="tax"> </span><br/>
                         <label> 계산된 금액 : </label><span id="total"> </span>
