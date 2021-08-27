37b5202c8d2400172a9b30bd76c3e34495d0c665
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-27 15:16:18 +0900

    땅따먹기 내역 수정요청
    요청자 : 노성래

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -145,9 +145,10 @@
                 for(var i=1; i<no; i++){
                     if(typeof($.cookie('menu_array')) != 'undefined'){
                         var check_cookie = false;
-                        for(var j=1;j<=menu_cookie_array.length;j++){   //장바구니에 추가된 상품일경우
+                        for(var j=1;j<menu_cookie_array.length;j++){   //장바구니에 추가된 상품일경우
                             if(menu_cookie_array[j] == menu_idx_array[i]){
                                 menu_cnt_string += "/"+(Number(cnt_cookie_array[j])+Number($("#cnt_"+menu_idx_array[i]).val()));
+                                menu_idx_string += "/"+menu_idx_array[i];
                                 check_cookie = true;
                             }else{
                                 menu_idx_string += "/"+menu_cookie_array[j];
