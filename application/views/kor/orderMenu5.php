c1fe6f490607d7a28c8f4e913a090554402f675e
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-08-27 13:23:58 +0900

    test

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -155,6 +155,8 @@
                 $.cookie('menu_array', menu_idx_array, {path: '/' });
                 $.cookie('cnt_array', menu_cnt_array, {path: '/' });
                 return;
+                
+                alert('test');
             }
         }
         
