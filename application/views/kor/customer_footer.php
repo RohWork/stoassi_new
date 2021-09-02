f85d0b2f9b5dba894de1b99bc0382e8445ccfccc
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-09-03 08:50:41 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/customer_footer.php
+++ application/views/kor/customer_footer.php
@@ -7,7 +7,7 @@
             <div class="col-md-1"></div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="location.href='/customer/orderMenu/1/10'">HOME</div>
             <div class="col-md-2 col-xs-2" style="text-align: center" onclick="history.back();">뒤로가기</div>
-            <div class="col-md-2 col-xs-3" style="text-align: center" onclick="$('#myModal').modal('show');">장바구니</div>
+            <div class="col-md-2 col-xs-3" style="text-align: center" onclick="$('#myModal').modal('show');check_basket();">장바구니</div>
             <div class="col-md-2 col-xs-5">
                 <form method="post" id="change_form" action="<?=explode("?",$_SERVER['REQUEST_URI'])[0]?>">
                     <select class="form-control" id="lang_change" name="language" onchange="this.form.submit()">
