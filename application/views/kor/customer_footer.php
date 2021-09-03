c82600cf7259459e163950517bac3bc772ed7988
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-09-03 13:05:51 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/customer_footer.php
+++ application/views/kor/customer_footer.php
@@ -40,7 +40,7 @@
           <table id="basket_list" style="width:100%;" class="table">
               <tr>
                   <th width="10%"></th>
-                  <th width="40%">메뉴명</th>
+                  <th width="45%">메뉴명</th>
                   <th width="20%">금액</th>
                   <th width="20%">갯수</th>
                   <th width="5%"></th>
