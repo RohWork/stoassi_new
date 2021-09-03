2a33281baf61988dc87003f34c0c47b5e6b3a7af
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-09-03 09:44:41 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/customer_footer.php
+++ application/views/kor/customer_footer.php
@@ -46,8 +46,10 @@
           </table>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        
         <button type="button" class="btn btn-primary">구입</button>
+        <button type="button" class="btn btn-primary">삭제</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
