32f892716a516df2473b2476a99ef0cb63b991b6
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-09-03 10:56:40 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/customer_footer.php
+++ application/views/kor/customer_footer.php
@@ -104,7 +104,7 @@
                         for(var i=0; i<basket.length; i++){
                             
                             $("#basket_list").append("<tr class='value_tr'>"
-                                                        +"<td> <input type='checkbox' name='basket_idx' id='basket_idx_"+basket[i].idx+"' value='"+basket[i].idx+"'/></td>"
+                                                        +"<td> <input type='checkbox' name='basket_idx' id='basket_idx_"+basket[i].idx+"' value='"+basket[i].idx+"' checked/></td>"
                                                         +"<td>"+basket[i].name+"</td>"
                                                         +"<td> <input type='number' name=basket_cnt[]' class='form-control' value='"+basket_array[basket[i].idx]+"'/></td>"
                                                         +"<td> <input type='button' onclick='remove_basket("+basket[i].idx+")' value='삭제' class='form-control btn-danger'/></td>"+
