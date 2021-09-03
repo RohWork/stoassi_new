8efb59fc5a5a61b72b81fa7cbe99f39ed611358c
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-09-03 10:55:44 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/customer_footer.php
+++ application/views/kor/customer_footer.php
@@ -132,9 +132,6 @@
     
         var del_idx = menu_array.indexOf(""+idx);
         
-        console.log(idx);
-        console.log(menu_array);
-        
         if(del_idx != -1){
             var menu_cnt_string = "";
             var menu_idx_string = "";
