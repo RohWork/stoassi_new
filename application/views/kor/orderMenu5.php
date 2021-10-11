db43e34d75408e8c1ee99758cf07e3b8e5dd93af
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-10-11 13:18:09 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -121,7 +121,7 @@
                             "<td>"+priceArray[idx]+"</td>"+
                         "<td>"+
                         "<button type='button' > + </button>"+
-                        "<input type='text' maxlength='3' id='cnt_"+idx+"' name='cnt_"+idx+"' class='form-control' readonly onKeyup='calcPrice()' value='1' />"+
+                        "idx"+
                         "<button type='button' > - </button>"+
                         "</td></tr>"
                 );
