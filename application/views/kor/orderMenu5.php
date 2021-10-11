f405910795c216033c1774b4f22ade5d8ff21a2e
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-10-11 13:14:40 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/orderMenu5.php
+++ application/views/kor/orderMenu5.php
@@ -120,9 +120,9 @@
                             "<td>"+menuArray[idx]+"</td>"+
                             "<td>"+priceArray[idx]+"</td>"+
                         "<td>"+
-                        "<button type='button' class='btn btn-primary'>+</button>"+
+                        "<button type='button' class='btn btn-primary'> + </button>"+
                         "<input type='text' style='width:100px' id='cnt_"+idx+"' name='cnt_"+idx+"' class='form-control' readonly onKeyup='calcPrice()' value='1' />"+
-                        "<button type='button' class='btn btn-primary'>-</button>"+
+                        "<button type='button' class='btn btn-primary'> - </button>"+
                         "</td></tr>"
                 );
                 priceIdxArray.push(idx);
