f4e8d40bbdaaf7128c7d9111921fbcb3a65faf10
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-10-13 15:10:54 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -107,6 +107,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             $vo->table_no = $this->input->post("table_no");
             $vo->status = $this->input->post("table_mode");
             $vo->shop_idx = $this->session->userdata("shop_idx");
+            $vo->table_code = md5(date('his'));
             
             $result = $this->table_md->set_table($vo);
             
