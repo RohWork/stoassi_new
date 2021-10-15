82b22a1f862d388569ac526e53cc9a6d2f3dcfc8
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-10-15 09:37:27 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/views/kor/order_list_counter.php
+++ application/views/kor/order_list_counter.php
@@ -8,7 +8,7 @@
 			<table class="table">
 				<thead>
 					<tr>
-						<th>NO</th>
+						<th>테이블번호</th>
 						<th>테이블코드</th>
                                                 <th>테이블상태</th>
                                                 <th>주문금액</th>
@@ -23,7 +23,7 @@
 				foreach($order_list as $row){		
 				?>
 					<tr>
-						<td><?=$no?></td>
+						<td><?=$row->table_no > 0 ? $row->table_no : "포장" ?></td>
 						<td><?=$row->table_code?></td>
                                                 <td><?=$row->status == 1 ?  "비어있음" : "사용중" ?></td>
                                                 <td><?=$row->price?> <?= !empty($row->price) ?  "zł" : "" ?></td>
