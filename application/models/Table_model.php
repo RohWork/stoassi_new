901ade26f7314d83d5c3bc20dbe95da047e4d822
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-10-15 10:04:09 +0900

    레벨 테스트 수정
    요청자 : 노성래

--- application/models/Table_model.php
+++ application/models/Table_model.php
@@ -21,6 +21,8 @@
             
             $this->db->order_by("ti.idx", "desc");
 
+            echo  $this->db->last_query();
+            
             return $this->db->get()->result();
 
         }
