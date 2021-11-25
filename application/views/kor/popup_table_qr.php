4e5c060312eb8e18b5e43bc30a8f8bb8fea266fe
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 10:40:58 +0900

    레벨테스트 수정
    요청자 : 제혜민

---
+++ application/views/kor/popup_table_qr.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <!-- The above 2 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <title>Refobi</title>
+            <!-- 합쳐지고 최소화된 최신 CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+            <!-- 부가적인 테마 -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <!-- 쿠키 사용 --> 
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script> 
+
+            <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+            <!-- Custom styles for this template --> 
+            <link href="../../assets/css/sticky-footer-navbar.css" rel="stylesheet">
+    </head>
+    <body>  
+        <div class="container">
+            <div class="page-header">
+                    <h1>주문관리</h1>
+                    <p class="lead">주문관리 화면 - 주방</p>
+            </div>
+            <div class='row'>
+                <div class='col-sm-12'>
+                    <img src='<?=$file?>'/>
+                </div>
+            </div>
+
+        </div>
+</body>
+</html>
