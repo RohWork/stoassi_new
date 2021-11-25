59a3aea34a18997748b1e2c39a1505b98958fb24
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 15:40:18 +0900

    레벨테스트 수정
    요청자 : 제혜민

--- application/views/kor/order_list_wait.php
+++ application/views/kor/order_list_wait.php
@@ -1,5 +1,25 @@
 <html>
-    <head></head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- The above 2 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Refobi</title>
+        <!-- 합쳐지고 최소화된 최신 CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+        <!-- 부가적인 테마 -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- 쿠키 사용 --> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script> 
+
+        <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <!-- Custom styles for this template --> 
+        <link href="../../assets/css/sticky-footer-navbar.css" rel="stylesheet">
+    </head>
     <body>
         <table style="width:100%;" cellpadding='0' border=0'' cellspacing='0'>
             <tr style='border-bottom: 1px solid black'>
