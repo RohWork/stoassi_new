411c2d7f0b2188d91bff4ade231b4b4c39a7467a
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 15:46:34 +0900

    레벨테스트 수정
    요청자 : 제혜민

--- application/views/kor/order_list_wait.php
+++ application/views/kor/order_list_wait.php
@@ -29,7 +29,7 @@
                 <th>갯수</th>
                 <th>주문일시</th>
             </tr>
-            <tr style='height:30px;line-height: 30px;padding-left: 5px'>
+            <tr style='height:30px;line-height: 30px;padding-left: 15px'>
                 <td><input type='checkbox'/></td>
                 <td>1</td>
                 <td>1</td>
