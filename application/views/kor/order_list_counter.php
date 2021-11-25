3380c70471b6e1d9e982f774e1843f7df1f386ac
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 10:55:51 +0900

    레벨테스트 수정
    요청자 : 제혜민

--- application/views/kor/order_list_counter.php
+++ application/views/kor/order_list_counter.php
@@ -226,7 +226,7 @@ echo get_qr("www.naver.com",'naver');
 			<form id="table_set_form" enctype="multipart/form-data" class="form-horizontal">
                             <div class="form-group">
                                 <div class="col-sm-12" style="text-align: center">
-                                        <img id='qr_code_img' style='width:150px; height:150px'/>
+                                        <img id='qr_code_img' style='width:250px; height:250px'/>
                                     </div>
 
                             </div>
