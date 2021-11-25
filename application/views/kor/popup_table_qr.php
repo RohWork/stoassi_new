e74896e561864f5dd7a6ca1661d652b00c5f589f
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 10:43:20 +0900

    레벨테스트 수정
    요청자 : 제혜민

--- application/views/kor/popup_table_qr.php
+++ application/views/kor/popup_table_qr.php
@@ -22,12 +22,8 @@
     </head>
     <body>  
         <div class="container">
-            <div class="page-header">
-                    <h1>주문관리</h1>
-                    <p class="lead">주문관리 화면 - 주방</p>
-            </div>
             <div class='row'>
-                <div class='col-sm-12'>
+                <div class='col-sm-12' style="text-align: center">
                     <img src='<?=$file?>'/>
                 </div>
             </div>
