250abbfeaee4e99bbc39d1ace2404686807f0df7
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 10:39:40 +0900

    레벨테스트 수정
    요청자 : 제혜민

---
+++ application/views/kor/pop_table_qr.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="page-header">
+            <h1>주문관리</h1>
+            <p class="lead">주문관리 화면 - 주방</p>
+    </div>
+    <div class='row'>
+        <div class='col-sm-12'>
+            <img src='<?=$file?>'/>
+        </div>
+    </div>
+    
+</div>
