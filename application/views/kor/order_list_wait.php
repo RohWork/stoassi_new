aca4e2df7eeb79f138d30c63d43ec1092c9da0fb
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 15:48:53 +0900

    레벨테스트 수정
    요청자 : 제혜민

--- application/views/kor/order_list_wait.php
+++ application/views/kor/order_list_wait.php
@@ -33,7 +33,7 @@
     <body>
         <table style="width:100%;" cellpadding='0' border='0' cellspacing='0'>
             <tr style='border-bottom: 1px solid black'>
-                <th><input type='checkbox'/></th>
+                <th style='padding-left:10px;'><input type='checkbox'/></th>
                 <th>상품명</th>
                 <th>상품가격</th>
                 <th>갯수</th>
