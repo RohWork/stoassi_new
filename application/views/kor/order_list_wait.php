2976d1884c456ee6d3415f83be8d701010241d12
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-11-25 15:32:10 +0900

    레벨테스트 수정
    요청자 : 제혜민

---
+++ application/views/kor/order_list_wait.php
@@ -0,0 +1,16 @@
+<html>
+    <head></head>
+    <body>
+        <table>
+            <tr>
+                <th><input type='checkbox'/></th>
+                <th>상품명</th>
+                <th>상품가격</th>
+                <th>갯수</th>
+                <th>주문일시</th>
+            </tr>
+            <tr></tr>
+        </table>
+    </body>
+    
+</html>
