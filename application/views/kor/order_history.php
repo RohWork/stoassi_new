bce3a21185e39e3c435c784d92d0c0eff4bf67d6
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-01-07 13:39:08 +0900

    lms 페이지 수정

--- application/views/kor/order_history.php
+++ application/views/kor/order_history.php
@@ -6,12 +6,14 @@ echo get_qr("www.naver.com",'naver');
 		<div class="page-header">
 			<h1>주문관리</h1>
 			<p class="lead">주문내역</p>
-		</div>
-            <div class="col-sm5">
-                    <button id="excel_print" class="btn btn-primary">excel</button>
+                        <div class="rows">
+                            <div class="col-sm-1">
+                                <button id="excel_print" class="btn btn-primary">excel</button>
                     <input type="date" id="sdate" name="sdate" style="margin-left:30px"/>~
                     <input type="date" id="edate" name="edate"/>
-                </div>
+                            </div>
+                        </div>
+		</div>
 		<div class="table-responsive">
 			<table class="table">
 				<thead>
