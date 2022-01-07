bd066c63f943878a413c8b08f646852249dec3c4
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-01-07 14:16:11 +0900

    lms 페이지 수정

--- application/views/kor/order_history.php
+++ application/views/kor/order_history.php
@@ -11,7 +11,7 @@ echo get_qr("www.naver.com",'naver');
                                 <button type="button" id="excel_print" class="btn btn-primary">excel</button>
                                 <input type="date" id="sdate" name="sdate" style="margin-left:100px"/>~
                                 <input type="date" id="edate" name="edate"/>
-                                <button id="search" name="search" class="btn btn-primary">검색</button>
+                                <button type="button" id="search" name="search" class="btn btn-primary">검색</button>
                             </form>
                         </div>
 		</div>
@@ -67,7 +67,9 @@ echo get_qr("www.naver.com",'naver');
          $("#sdate").val(new Date().toISOString().substring(0, 10));
          $("#edate").val(new Date().toISOString().substring(0, 10));
     
-        
+        $("#search").click(function(){
+           $("#frm").submit();
+        });
     });
 </script>
 </html>
