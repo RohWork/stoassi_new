ff46efa3f612e2dc2e3ed91554d8f5741b85b0a9
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-07-05 14:24:38 +0900

    히스토리 데이터 출력

--- application/controllers/StockHistory.php
+++ application/controllers/StockHistory.php
@@ -17,8 +17,22 @@ class StockHistory extends CI_Controller {
             
             $where = "";
             
+            $search_vo  = new stdClass();
+
+            $config['per_page'] = 10;
+            $offset = $this->input->get('per_page');
+            $config['base_url'] = current_url() . '?' . reset_GET('per_page');
+
+            $search_vo->config_per_page = $config['per_page'];
+            $config['total_rows'] = $this->stock_md->count_stock_history($search_vo);
+
+            $config = setPagination($config);
+            $this->pagination->initialize($config);
+
+            $data['pagination'] = $this->pagination->create_links();
             
-            $data['stock_history'] = $this->stock_md->get_stock_history_array($where);
+            
+            $data['stock_history'] = $this->stock_md->get_stock_history_array($search_vo);
             
             $this->load->view(LANGUAGE.'/header', $this->head_data);
             $this->load->view(LANGUAGE.'/stock_history_list', $data);
