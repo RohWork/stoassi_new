3686ebcd31afbbdd8a91f5334b6fa359fa4c4d6c
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-07-19 08:22:14 +0900

    셀프테스트 추가 작업

---
+++ application/models/Schedule_model.php
@@ -0,0 +1,15 @@
+<?php
+class Schedule_model extends CI_Model {
+	
+    public function __construct() {
+        parent::__construct();
+        $this->load->database('default', TRUE);
+    }
+    
+    function get_schedule($user_idx){
+        
+        
+        
+    }
+    
+}
