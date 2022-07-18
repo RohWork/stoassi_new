0aafd76d22a5937a130c439261b835661fc8b969
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-07-19 08:22:16 +0900

    셀프테스트 추가 작업

---
+++ application/controllers/Schedule.php
@@ -0,0 +1,26 @@
+<?php
+class Schedule extends CI_Controller {
+	
+	function __construct() {
+            parent ::__construct();
+
+            $this->head_data = header_set("main");
+            $this->load->model('Member_model', 'member_md', TRUE);
+            $this->load->model('Shop_model', 'shop_md', TRUE);
+            $this->load->model('Schedule_model', 'schedule_md', TRUE);
+	}
+        
+        public function index(){
+            
+            $shop_idx = $this->session->userdata('shop_idx');
+            $user_idx = $this->session->userdata('user_idx');
+
+            
+            
+            
+            $this->load->view(LANGUAGE.'/header', $this->head_data);
+            $this->load->view(LANGUAGE.'/schedule_calendar', $data);
+            
+        }
+}
+?>
