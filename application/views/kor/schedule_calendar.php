f93ef7c1ca096068377ee16b15f558bef4442131
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-07-19 16:06:52 +0900

    셀프테스트 추가 작업

--- application/views/kor/schedule_calendar.php
+++ application/views/kor/schedule_calendar.php
@@ -134,7 +134,6 @@
                 
                 var month_data = ("0" + (today.getMonth() + 1)).slice(-2);
                 
-                console.log(today.getFullYear()+'-'+month_data);
                 $.ajax({
                     url:'/Schedule/get_month_schedule',
                     type:'post',
