031a3c5eba0ed611ee54a9f4d8c7e05505dde1f6
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-07-19 15:35:41 +0900

    셀프테스트 추가 작업

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -6,9 +6,16 @@ class Schedule_model extends CI_Model {
         $this->load->database('default', TRUE);
     }
     
-    function get_schedule($user_idx){
+    function get_schedule($user_idx, $shop_idx, $date){
         
+        $this->db->select('cd.date, cd.state');
+        $this->db->select('(SELECT COUNT(*) FROM commute_time AS ct WHERE cd.idx = ct.date_idx AND ct.state = 1) AS time_cnt');    
+        $this->db->select('(SELECT COUNT(*) FROM commute_time AS ct2 WHERE cd.idx = ct2.date_idx AND ct2.member_idx = '.$user_idx.') AS use_cnt ');
         
+        $this->db->from('commute_date as cd');
+        $this->db->like('cd.date', $date, 'after');
+        
+        return $this->db->get()->result();
         
     }
     
