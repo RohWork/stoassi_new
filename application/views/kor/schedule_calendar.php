0ffe432cf9809093c223940c947dff94c460045e
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-07-20 11:02:23 +0900

    셀프테스트 추가 작업

--- application/views/kor/schedule_calendar.php
+++ application/views/kor/schedule_calendar.php
@@ -180,10 +180,10 @@
         for(var i=0; i<sche.length; i++){
             
             var day = sche[i].date.split('-')[2];
-            console.log(day);
+
             if(sche.state = 1){
-                if(sche.time_cnt > 0){   //신청대기중인 일정이 1개이상인경우
-                    if( sche.use_cnt = 0){   //해당날짜에 본인이 신청한 일정이 없는경우
+                if(Number(sche.time_cnt) > 0){   //신청대기중인 일정이 1개이상인경우
+                    if( Number(sche.use_cnt) = 0){   //해당날짜에 본인이 신청한 일정이 없는경우
                         sch_array[day] =   "<span class='sch_wait'>신청가능</span>";
                     }else{  //해당날짜에 본인이 신청한 일정이 하나 이상인 경우
                         sch_array[day] = "<span class='sch_complete'>신청완료</span>";
